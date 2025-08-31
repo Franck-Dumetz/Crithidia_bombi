869f9390b56ab4985bf7010dab4506fc43b93aff
Franck Dumetz <105318680+Franck-Dumetz@users.noreply.github.com>
2025-08-31 15:49:28 -0400

    Add files via upload

---
+++ Annotation/Minimap_ONT.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=Cb_map                      # Job name
+#SBATCH --output=/local/projects-t3/SerreDLab-3/fdumetz/Crithidia/ONT/ONT_DRS.out   # Standard output log
+#SBATCH --error=/local/projects-t3/SerreDLab-3/fdumetz/Crithidia/ONT/ONT_DRS.err    # Standard error log
+#SBATCH --mail-type=BEGIN,END --mail-user=fdumetz@som.umaryland.edu
+#SBATCH --cpus-per-task=32				# Number of CPUs per task
+#SBATCH --mem=30G                              # Memory per node
+
+
+minimap2 -ax map-ont -k14 -t 32 /local/projects-t3/SerreDLab-3/fdumetz/Crithidia/hifi/Cbombi_genome_refined.fasta /local/projects-t3/SerreDLab-3/fdumetz/Crithidia/ONT/Cbombi_swimming_ONT.fastq.gz > /local/projects-t3/SerreDLab-3/fdumetz/Crithidia/ONT/Cbombi_ONT.sam
+
