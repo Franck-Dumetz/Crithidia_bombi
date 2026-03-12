15dbdd110170204418aa7c69cb66ff119c200cb4
Franck Dumetz <105318680+Franck-Dumetz@users.noreply.github.com>
2026-03-12 14:57:00 -0400

    Update output paths and email in hifi_slurm.sh

--- genome_assembly/hifi_slurm.sh
+++ genome_assembly/hifi_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=Cbombi_asm                     # Job name
-#SBATCH --output=/local/projects-t3/SerreDLab-3/fdumetz/Crithidia/hifi/Cbhifiasm.out   # Standard output log
-#SBATCH --error=/local/projects-t3/SerreDLab-3/fdumetz/Crithidia/hifi/Cbhifiasm.err    # Standard error log
-#SBATCH --mail-type=BEGIN,END --mail-user=fdumetz@som.umaryland.edu
+#SBATCH --output=/path/Cbhifiasm.out   # Standard output log
+#SBATCH --error=/path/Cbhifiasm.err    # Standard error log
+#SBATCH --mail-type=BEGIN,END --mail-user=email
 #SBATCH --cpus-per-task=32				# Number of CPUs per task
 #SBATCH --mem=700G                                        # Memory per node
 
 
-/usr/local/packages/hifiasm-0.24.0/bin/hifiasm -o Cb_hifi.asm -t32 --telo-m TTAGGG --dual-scaf /local/projects-t3/SerreDLab-3/fdumetz/Crithidia/Cbombi_hifi_filtered.fastq.gz
+/usr/local/packages/hifiasm-0.24.0/bin/hifiasm -o Cb_hifi.asm -t32 --telo-m TTAGGG --dual-scaf /path/Cbombi_hifi_filtered.fastq.gz
